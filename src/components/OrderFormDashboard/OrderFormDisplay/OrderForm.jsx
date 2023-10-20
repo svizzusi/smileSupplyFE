a32fb3b7108ec1bf9548db0b655041fac284240b
Stephen Vizzusi <svizzusi13@gmail.com>
2023-10-20 08:38:37 -0700

    Added jsPDF for order form using useRef

--- src/components/OrderFormDashboard/OrderFormDisplay/OrderForm.jsx
+++ src/components/OrderFormDashboard/OrderFormDisplay/OrderForm.jsx
@@ -1,17 +1,18 @@
 import style from './OrderForm.module.css'
 import OrderFormCard from "./OrderFormCard";
 import OrderFormHeader from './OderFormHeader'
-import { useState, useEffect } from 'react'
-import { HiDocumentDownload } from 'react-icons/hi';
-import { GrPowerReset } from 'react-icons/gr';
-import axios from 'axios';
-import html2canvas from 'html2canvas';
+import {useState, useEffect, useRef} from 'react'
+import {HiDocumentDownload} from 'react-icons/hi';
+import {GrPowerReset} from 'react-icons/gr';
+import axios from 'axios'
 import jsPDF from 'jspdf';
 
 const OrderForm = ({ setShowEditProduct, setProductId, order, setOrder, toast }) => {
   const [totalPrice, setTotalPrice] = useState(0);
   const [orderIds, setOrderIds] = useState([]);
 
+
+ 
   const removeAllOrders = async () => {
     try {
       for (const id of orderIds) {
@@ -19,11 +20,11 @@ const OrderForm = ({ setShowEditProduct, setProductId, order, setOrder, toast })
         await axios.put(`https://odd-gold-anemone-cap.cyclic.app/products/resetFrequency/${id}`);
         console.log(`Order removed for product with ID: ${id}`);
       }
-
+  
       // Clear the orderIds
       setOrderIds([]);
       setTimeout(() => {
-        window.location.reload();
+        location.reload();
       }, 500);
     } catch (error) {
       // Handle any errors here
@@ -31,53 +32,55 @@ const OrderForm = ({ setShowEditProduct, setProductId, order, setOrder, toast })
     }
   };
 
+  useEffect(() => {
+    console.log(orderIds);
+  }, [orderIds]);
+
+  const tableRef = useRef(null)
+
   const generatePDF = () => {
-    const pdf = new jsPDF();
-    const pdfContainer = document.getElementById('pdf-container');
+    const pdf = new jsPDF("p", "pt");
+    const tableContainer = tableRef.current
+    
+    pdf.addFont("helvetica", "normal");
 
-    if (pdfContainer) {
-      html2canvas(pdfContainer).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        pdf.addImage(imgData, 'PNG', 15, 15, pdfContainer.clientWidth / 2, pdfContainer.clientHeight / 2);
-        pdf.save('table.pdf');
-      }).catch((error) => {
-        console.error('Error generating PDF:', error);
-      });
-    } else {
-      console.error("PDF container element not found.");
-    }
+    pdf.html(tableContainer, 15, 15);
+  
+    pdf.save('table.pdf');
   };
+  
 
-  return (
-    <section className={style.orderFormSection}>
-      <div>
-        <h2 className={style.orderFormHeading}>Order Form</h2>
-        {/* ... Your other content ... */}
-      </div>
-      <table className={style.orderFormTable} id="pdf-container">
-        <OrderFormHeader />
-        <OrderFormCard setShowEditProduct={setShowEditProduct} setProductId={setProductId}
-          setOrderIds={setOrderIds}
-          order={order} setOrder={setOrder} toast={toast} setTotalPrice={setTotalPrice} />
-      </table>
-      <section className={style.orderFormTotal}>
-        <h3>Grand Total: ${totalPrice}</h3>
-      </section>
-      <div className={style.orderFormButtons}>
-        <button
-          onClick={generatePDF}
-          className={style.orderFormBtns}
-        ><span>Download Order Form</span>
-          <HiDocumentDownload />
-        </button>
-        <button
-          className={style.orderFormBtns}
-          onClick={removeAllOrders}
-        ><span>Reset Order Form</span> <GrPowerReset /></button>
-      </div>
-    </section>
-  )
-};
+    return (
+        <section className={style.orderFormSection}>
+          <div>
+            <h2 className={style.orderFormHeading}>Order Form</h2>
+            <p className={style.orderFormParagraph}>Welcome to the Smile Supply Order Form, your gateway to effortless dental inventory management. Our intuitive order form simplifies the process with just a few clicks.</p> 
+            <p className={style.orderFormParagraph}>Start by selecting the product you need, and with a single click, it's copied to your clipboard and highlighted in tan, ensuring you don't double up on items. Next, hop over to your preferred supplier's website and paste the product ID to add it to your shopping cart – no more manual data entry hassles. Repeat this for each item on your order list, and when you're done, simply hit the "Clear Order Form" button on Smile Supply. This action resets product order frequencies to your specified preferences and readies the form for your next order. Streamline your dental inventory management with Smile Supply – it's that easy! Enjoy the convenience of a well-organized and efficient ordering process.</p>
+          </div>
+          <table className={style.orderFormTable} ref={tableRef}>
+            <OrderFormHeader />
+            <OrderFormCard setShowEditProduct={setShowEditProduct} setProductId={setProductId}
+             setOrderIds={setOrderIds} 
+            order={order} setOrder={setOrder} toast={toast} setTotalPrice={setTotalPrice}/>     
+          </table>
+          <section className={style.orderFormTotal}>
+            <h3>Grand Total: ${totalPrice}</h3>
+          </section>
+          <div className={style.orderFormButtons}>
+            <button 
+              onClick={generatePDF}
+              type="primary"
+              className={style.orderFormBtns}
+              ><span>Download Order Form</span> 
+              <HiDocumentDownload/>
+            </button>
+            <button 
+              className={style.orderFormBtns}
+              onClick={removeAllOrders}
+            ><span>Reset Order Form</span> <GrPowerReset/></button>
+          </div>
+        </section>
+      )
+    };
 
 export default OrderForm;
-
