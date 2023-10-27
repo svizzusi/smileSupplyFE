0f4d5668c85a2f6f5b329e19c7b0323f35d93643
Stephen Vizzusi <svizzusi13@gmail.com>
2023-10-27 12:50:48 -0700

    VITE_VERCEL_

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './reset.css'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const clientID = import.meta.env.Vercel_GOOGLE_CLIENT_ID
+const clientID = process.env.Vite_VERCEL_GOOGLE_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientID}>
