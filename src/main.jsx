51e02a4393746fd769a26313dc3ad671673725a7
Stephen Vizzusi <svizzusi13@gmail.com>
2023-10-27 12:34:38 -0700

    Changed to process.env

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './reset.css'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const clientID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+const clientID = process.env.VITE_GOOGLE_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientID}>
